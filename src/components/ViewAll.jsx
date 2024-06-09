16fdfb77aa8ce3b097c8426594e71d2149df88c9
Annaeldho01 <annaeldho001@gmail.com>
2024-06-09 23:41:56 +0530

    project completed

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ViewAll = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">SL.NO</th>
+      <th scope="col">ID</th>
+      <th scope="col">TODO</th>
+      <th scope="col">USED</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>101</td>
+      <td>ASFF GM</td>
+      <td>GFGHH</td>
+    </tr>
+  </tbody>
+</table>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAll
